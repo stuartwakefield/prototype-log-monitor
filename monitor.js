e53917071221ca8287285f7aea275f14d735378e
Stuart Wakefield <stuie@live.co.uk>
2013-02-11 22:47:09 +0000

    Some updates

--- monitor.js
+++ monitor.js
@@ -2,23 +2,27 @@ var WebSocketServer = require("websocket").server
   , http = require("http")
   , ampq = require("amqp");
 
+// Create a HTTP server to switch up to Web Sockets
 var server = http.createServer(function(req, res) {
 	res.writeHead(404);
 	res.end();
 });
 server.listen(8080);
 
+// Create a Web Socket server to upgrade from HTTP
+var websocket = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnection: false
+});
+
+// Create a connection to the message queue
 var conn = ampq.createConnection(
 		{ host: "localhost"
 		, port: 9132 }
 	);
 
+// Systems will be connected through events
 var updates = new EventEmitter();
-
-var websocket = new WebSocketServer({
-	httpServer: server,
-	autoAcceptConnection: false
-});
 
 websocket.on("request", function(req) {
 
