4850857e9022385d64d368d04eaecc63b999c4b6
Stuart Wakefield <stuie@live.co.uk>
2013-02-13 12:39:34 +0000

    General updates to prototype, is working on local system. Untested

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Log Monitor Prototype
 
-Building prototype for a log monitor using ZeroMQ and NodeJS.
+Building prototype for a log monitor using RabbitMQ and NodeJS. Would
+be nice to have this in Clojure and ZeroMQ...
+
+Usage
+
+    node watch.js /path/to/file.log
+
+Then
+
+    node monitor.js
+
+Serve up the monitor.html file (will add this to the server some time)
+
+Add more watchers, add more monitors, add more clients... Have fun
--- monitor.js
+++ monitor.js
@@ -1,56 +1,11 @@
-var WebSocketServer = require("websocket").server
-  , http = require("http")
-  , ampq = require("amqp");
+var EventEmitter = require("events").EventEmitter
+  , Subscriber = require("./lib/monitor/subscriber")
+  , Server = require("./lib/monitor/server");
 
-// Create a HTTP server to switch up to Web Sockets
-var server = http.createServer(function(req, res) {
-	res.writeHead(404);
-	res.end();
-});
-server.listen(8080);
-
-// Create a Web Socket server to upgrade from HTTP
-var websocket = new WebSocketServer({
-	httpServer: server,
-	autoAcceptConnection: false
-});
-
-// Create a connection to the message queue
-var conn = ampq.createConnection(
-		{ host: "localhost"
-		, port: 9132 }
-	);
+var host = "127.0.0.1";
+var port = 8080;
 
 // Systems will be connected through events
-var updates = new EventEmitter();
-
-websocket.on("request", function(req) {
-
-	var conn = req.accept("echo-protocol", req.origin);
-
-	var receive = function(msg) {
-		conn.send(msg);
-	};
-
-	updates.on("update", receive);
-
-	conn.on("message", function(msg) {
-		// Do nothing
-	});
-
-	conn.on("close", function(code, desc) {
-		updates.removeListener("update", receive);
-	});
-
-});
-
-conn.on("ready", function() {
-	conn.queue(null, function(queue) {
-
-		queue.subscribe(function(msg) {
-			updates.emit("update", msg);
-		});
-
-		queue.bind("monitor");
-	});
-})
+var emitter = new EventEmitter();
+var subscriber = new Subscriber(host, emitter);
+var server = new Server(port, emitter);
